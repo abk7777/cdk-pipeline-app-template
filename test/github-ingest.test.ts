9cf5cd92708269c701077394f5bd03e938ec1604
Gregory Lindsey <gclindsey@gmail.com>
2023-05-11 21:27:14 -0700

    Initial commit

---
+++ test/github-ingest.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as GithubIngest from '../lib/github-ingest-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/github-ingest-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new GithubIngest.GithubIngestStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
