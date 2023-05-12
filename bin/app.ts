7950ab228b83ffdbd565c9cfe425c36f63f36fd8
Gregory Lindsey <gclindsey@gmail.com>
2023-05-11 22:38:56 -0700

    add tags

--- bin/app.ts
+++ bin/app.ts
@@ -7,9 +7,14 @@ import { AppStack } from '../lib/app-stack';
 import * as config from '../config.json'
 
 const app = new cdk.App();
-new AppStack(app, `${config.cdk_app.app_name}Stack`, `${config.cdk_app.app_stage}`, {
+const appStack = new AppStack(app, `${config.cdk_app.app_name}Stack`, `${config.cdk_app.app_stage}`, {
     env: {
         account: process.env.DEV_ACCOUNT_ID,
         region: process.env.AWS_REGION
     }
 });
+
+// add tags
+for (const [key, value] of Object.entries(config.cdk_app.tags)) {
+    cdk.Tags.of(appStack).add(key, value);
+}
--- lib/app-stack.ts
+++ lib/app-stack.ts
@@ -2,7 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 export class AppStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, stageName: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // code goes here
